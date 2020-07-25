55d236644457469fef46f145b29db1d1ba24f61c
Patrick McGuire <mcguirepr89@gmail.com>
2020-07-25 11:33:42 -0500

    Edited comments to not exceed 80 characters

--- Kitty-Fountain.ino
+++ Kitty-Fountain.ino
@@ -27,30 +27,30 @@ void setup() {
 
 //Main Loop
 void loop() {
-  delay(500);                        /* Wait 50ms between pings. 29ms should
-                                       be the shortest delay between pings*/
-  distance = sonar.ping_cm();       // Set the ping value to 'distance' variable
-  Serial.print(distance);           // Print 'distance' value in serial monitor
-  Serial.println("cm");             // Print 'distance' units in serial monitor
-  if ( distance == 0 ) {
-    digitalWrite(PUMP,LOW);
+  delay(500);                         /* Wait 50ms between pings. 29ms should
+                                         be the shortest delay between pings*/
+  distance = sonar.ping_cm();         // Set the ping value to 'distance'
+  Serial.print(distance);             // Print 'distance' value with units
+  Serial.println("cm");                  
+  if ( distance == 0 ) {              // If there is nothing in range...
+    digitalWrite(PUMP,LOW);           // be sure the pump is off
   }
-  if ( distance > 1 ) {             // If anyting is in range . . .
-    delay(50);
-    distance = sonar.ping_cm();
-    if ( distance > 1) {
+  if ( distance > 1 ) {               // If anyting is in range...
+    delay(50);                        // wait 50ms
+    distance = sonar.ping_cm();       // check the 'distance' variable again
+    if ( distance > 1) {              // If there is still something in range...
       digitalWrite(PUMP, HIGH);       // turn the pump on
-      delay(8000);
-    } else {                          // Else . . .
+      delay(8000);                    // for 8 seconds
+    } else {                          // Else...
       delay(50);                      // wait 50ms
-      distance = sonar.ping_cm();     // then check the 'distance' variable again
-      if ( distance == 0 ) {          // if nothing is in range . . .
+      distance = sonar.ping_cm();     // check the 'distance' variable again
+      if ( distance == 0 ) {          // if nothing is in range...
         delay(1000);                  // wait 1 second
-        distance = sonar.ping();      // then check the 'distance' variable again
-        if ( distance == 0 ) {        // if nothing is in range . . .
+        distance = sonar.ping();      // check the 'distance' variable again
+        if ( distance == 0 ) {        // if nothing is in range...
           delay(1000);                // wait 1 second
-          distance = sonar.ping();    // then check the 'distance' variable again
-          if ( distance == 0 ) {      // finally, if nothing is in range . . .
+          distance = sonar.ping();    // check the 'distance' variable again
+          if ( distance == 0 ) {      // finally, if nothing is in range...
             digitalWrite(PUMP, LOW);  // turn the pump off
           }
         }
